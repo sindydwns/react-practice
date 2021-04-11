3a76f96e3fd584e62b9ee5752851f6727ac8efeb
sindydwns <sindydwns@naver.com>
2021-04-11 21:12:29 +0900

    Update MultiCoinGraph.js

--- src/routers/MultiCoinGraph.js
+++ src/routers/MultiCoinGraph.js
@@ -8,16 +8,8 @@ async function loadCoinData(market, amount){
         .then(x => x.json());
 }
 
-function MiniLineChart({data, name}){
+function MiniLineChart({values, title, style}){
 
-    let values = (data || []);
-    values = (values.datasets) || [];
-    values = (values[0]) || [];
-    values = (values.data) || [];
-    const currentPrice = values[values.length - 1];
-    const max = Math.max(...values);
-    const min = Math.min(...values);
-    const percent = Math.round((max / min - 1) * 10000) / 100;
     const [option] = useState({
         // responsive: false,
         // legend: {display:false},
@@ -29,20 +21,34 @@ function MiniLineChart({data, name}){
         // elements: {line: {tension: 0}},
         responsive: false,legend: {display: false},elements: {line: {borderColor: '#000000',borderWidth: 1},point: {radius: 0}},tooltips: {enabled: false},scales: {yAxes: [{display: false}],xAxes: [{display: false}]}
     });
+    const data = {};
+    if(values){
+        data.labels = values.map(x => Math.random());
+        data.datasets = [{
+            label:"Trade Price",
+            data:values,
+            backgroundColor:"rgba(0,0,0,0.1)",
+        }];
+    }
 
     return (
-        <div>
-            <Line width={144} height={81} data={data} options={option}/>
-            <span>{name} {currentPrice} : {percent}%</span>
+        <div style={style}>
+            {title !== undefined ? <>
+                <Line width={144} height={81} data={data} options={option}/>
+                {/* <span>{key} {currentValue} : {data.percent}%</span> */}
+                <span>{title}</span>
+            </> : <div style={{width:144, height:100}}>loading...</div>
+            }
         </div>
     )
 }
 
 function MultiCoinGraph(){
 
-    const [coinArr] = useState(["PCI", "DKA", "ETH", "NPXS", "BTC", "XRP", "XEM", "CRO"]);
+    const [coinArr] = useState(["PCI", "DKA", "ETH", "PUNDIX", "BTC", "XEM", "CRO", "OMG"]);
     const [index, setIndex] = useState(0);
-    const [datas, setDatas] = useState(coinArr.map(_ => ({})));
+    const [datas, setDatas] = useState(coinArr.map(x => ({key:x})));
+    const [sorted, setSorted] = useState([]);
 
     useEffect(() => {
         const interval = setInterval(async() => {
@@ -51,27 +57,39 @@ function MultiCoinGraph(){
             const data = await loadCoinData("KRW-" + coin, 60);
             const reversedData = data.reverse();
             if(!data.error) {
-                const newData = {datasets:[]}
-                newData.labels = reversedData.map(x => new Date(x.candle_date_time_kst));
-                newData.datasets.push({
-                    label:"Trade Price",
-                    data:reversedData.map(x=>x.trade_price),
-                    backgroundColor:"rgba(0,0,0,0.1)",
-                });
+                const newData = {};
+                newData.values = reversedData.map(x=>+x.trade_price);
+                newData.max = Math.max(...newData.values);
+                newData.min = Math.min(...newData.values);
+                newData.percent = Math.round((newData.max / newData.min - 1) * 10000) / 100;
+                newData.key = coin;
+                newData.currentPrice = newData.values[newData.values.length - 1];
                 const a = datas.map(x => x);
                 a[index] = newData;
+                console.log(a.length);
                 setDatas(a)
             }
             setIndex(index + 1 >= coinArr.length ? 0 : index + 1)
         }, 200);
         return () => clearInterval(interval);
-    }, [coinArr, index, datas])
+    }, [coinArr, index, datas]);
+
+    useEffect(() => {
+        const dump = [...datas];
+        setSorted(dump.sort((x,y) => y.percent-x.percent));
+    }, [datas])
 
     return (
         <div className={menuStyle.main}>
             <h3>여러개의 가상화폐를 한 번에 표시합니다. </h3>
             <div className={style.flex}>
-                {coinArr.map((x, i) => <MiniLineChart key={i} data={datas[i]} name={x}/>)}
+                {datas.map((x,i) => x.values ? 
+                    <MiniLineChart
+                        key={x.key}
+                        values={datas.find(item=>item.key === x.key).values}
+                        title={`${x.key} ${x.currentPrice} : ${x.percent}%`}/>
+                    :<MiniLineChart key={x.key} />
+                )}
             </div>
         </div>
     )
